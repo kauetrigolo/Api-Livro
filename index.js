97c24565092e0510346d1a98780a4c65505c440d
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2023-10-12 17:30:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require('cors')
 // Declarando um limite de requisições para evitar ataques DDOS
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	limit: 300, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 	// store: ... , // Use an external store for more precise rate limiting
